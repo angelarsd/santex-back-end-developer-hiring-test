ba58839bf8525e6ecd02ce7aadbfe7986fd32ead
Ángel Silva <angel.silva@tiendanube.com>
2023-11-28 11:51:23 -0300

    (feat): add ImportLeagueService

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { ImportLeagueService } from './import-league.service';
 import { AppService } from './app.service';
+import { ValidateLeagueCodePipe } from './decorators';
+import { LeagueCodeType } from './types';
+import { ApiResponseInterface } from './interfaces';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly importLeagueService: ImportLeagueService,
+    private readonly appService: AppService,
+  ) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('importLeague')
+  async importLeague(
+    @Body('leagueCode', ValidateLeagueCodePipe) leagueCode: LeagueCodeType,
+  ): Promise<ApiResponseInterface> {
+    return this.importLeagueService.fetchAndSaveData(leagueCode);
   }
 }
