e8124d74fa20ab6c318a0bf2e698b3b7cf3ec0d3
Ángel Silva <angel.silva@tiendanube.com>
2023-11-28 11:48:30 -0300

    (feat): add decorators

---
+++ src/decorators/validate-league-code.decorator.ts
@@ -0,0 +1,18 @@
+import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
+import { LeagueCodeType, TLeagueCodeType } from 'src/types';
+
+@Injectable()
+export class ValidateLeagueCodePipe implements PipeTransform<string, string> {
+  transform(leagueCode: LeagueCodeType): string {
+    const allowedCodes = Object.values(TLeagueCodeType);
+    const allowedCodesText = allowedCodes.join(', ');
+
+    if (!allowedCodes.includes(leagueCode)) {
+      throw new BadRequestException(
+        `Invalid value for leagueCode. Allowed values are: ${allowedCodesText}`,
+      );
+    }
+
+    return leagueCode;
+  }
+}
