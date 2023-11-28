83ff0977178e99438a1f76f975a675103bc24450
Ángel Silva <angel.silva@tiendanube.com>
2023-11-28 12:34:29 -0300

    (fix): add parameter team

--- src/schemas/coaches.schema.ts
+++ src/schemas/coaches.schema.ts
@@ -4,12 +4,14 @@ export const CoachSchema = new mongoose.Schema({
   name: String,
   dateOfBirth: String,
   nationality: String,
+  team: String,
 });
 
 export interface CoachDto {
   name: string;
   dateOfBirth: string;
   nationality: string;
+  team: string;
 }
 
 export const CoachModel = mongoose.model<CoachDto & mongoose.Document>(
