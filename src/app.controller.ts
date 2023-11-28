dccc86ec17ebbe9a7cbea54c7b644cb68851ca77
Ángel Silva <angel.silva@tiendanube.com>
2023-11-28 20:25:54 -0300

    (feat): add new endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { ImportLeagueService } from './import-league.service';
-import { AppService } from './app.service';
+import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
+import { ImportLeagueService } from './services/import-league.service';
+import { PlayerService } from './services/player.service';
 import { ValidateLeagueCodePipe } from './decorators';
 import { LeagueCodeType } from './types';
-import { ApiResponseInterface } from './interfaces';
+import { ApiResponseInterface, TeamResponseInterface } from './interfaces';
+import { TeamService } from './services/team.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly importLeagueService: ImportLeagueService,
-    private readonly appService: AppService,
+    private readonly playerService: PlayerService,
+    private readonly teamService: TeamService,
   ) { }
 
   @Post('importLeague')
@@ -17,5 +19,25 @@ export class AppController {
     @Body('leagueCode', ValidateLeagueCodePipe) leagueCode: LeagueCodeType,
   ): Promise<ApiResponseInterface> {
     return this.importLeagueService.fetchAndSaveData(leagueCode);
+  }
+
+  @Get('players/:leagueCode')
+  async fetchPlayers(
+    @Param('leagueCode', ValidateLeagueCodePipe) leagueCode: LeagueCodeType,
+    @Query() queries: { name?: string },
+  ): Promise<any> {
+    return this.playerService.getPlayersByCompetition(leagueCode, queries);
+  }
+
+  @Get('team/:teamName')
+  async fetchTeam(
+    @Param('teamName') teamName: string,
+    @Query()
+    queries: {
+      includePlayers?: string;
+    },
+  ): Promise<ApiResponseInterface | TeamResponseInterface> {
+    const includePlayers = queries.includePlayers === 'true';
+    return this.teamService.findTeamByName(teamName, includePlayers);
   }
 }
